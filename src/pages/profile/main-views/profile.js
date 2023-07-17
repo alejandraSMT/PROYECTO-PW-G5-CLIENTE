873efae65a66083eb7d436b7aeb3409c557ce12b
gonzalo <131427451+20200735@users.noreply.github.com>
2023-07-17 00:39:55 -0500

    Update profile.js

--- src/pages/profile/main-views/profile.js
+++ src/pages/profile/main-views/profile.js
@@ -1,23 +1,64 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "../css/profile.css"
-
 import PersonalInfo from "../components/personal_info";
 import Tabs from "../components/tabs";
+import { useEffect, useState } from 'react';
 
-import Header from '../../Header/Header';
 
 function Perfil(){ //todo esto es la pantalla
+
+    const [personalInfo, setPersonalInfo] = useState([]);
+    const nombreUsu = "topesgesp";
+
+    useEffect(() => {
+        obtenerPersonalInfo();
+      }, []);
+
+
+    function obtenerPersonalInfo() {
+    fetch('http://localhost:3001/obtener-datos-info-personal/' + nombreUsu)
+        .then(response => response.json())
+        .then(data => {
+        console.log(data); // Verificar los datos obtenidos desde el servidor
+        setPersonalInfo(data);
+        })
+        .catch(error => console.log('Ocurrió un error:', error));
+    }
+
+    // function handleSubmit(parametros) {
+
+    //     const nombres = parametros.nombres;
+    //     const apellidos = parametros.apellidos;
+
+    //     fetch('http://localhost:3001/obtener-datos-info-personal/' + nombreUsu + '/' + nombres + '/' + apellidos, {
+    //     method: "post",
+    //     body: objJson,
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     })
+    //   .then(verificarEstado)
+    //   .then(() => {
+    //     window.alert("Usuario modificado");
+    //     myForm.reset();
+    //   })
+    //   .catch(handleError);
+    // }
+
+
+
+
     return(
-        <div>
-            <Header />
+        <div className="contenedorPerfil">
             <div id="cuerpo">
                 
                 <br></br>
                 <h1>
                     Mi perfil
                 </h1>
+                <button type="submit">Guardar</button>
                 <hr></hr>
-                <PersonalInfo />
+                <PersonalInfo personalInfo = {personalInfo} />
                 <br></br>
                 <Tabs />
             </div>
