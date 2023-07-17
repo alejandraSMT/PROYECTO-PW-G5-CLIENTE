39bd9ecfce81d1d49171f7995f7f40cc37bce7b6
gonzalo <131427451+20200735@users.noreply.github.com>
2023-07-17 14:10:13 -0500

    Update tab_universidad.js

--- src/pages/profile/components/tab_universidad.js
+++ src/pages/profile/components/tab_universidad.js
@@ -1,43 +1,57 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 
-
-const Universidad = () => {
+const Universidad = ({ formData, handleInputChange }) => {
   return (
     <div className="Universidad">
-      <div class="container">
-        <div class="izquierda_uni">
-          <div class="row">
-              <div class="col">
+      <div className="container">
+        <div className="izquierda_uni">
+          <div className="row">
+            <div className="col">
               <Form.Group>
-                  <Form.Label>Universidad</Form.Label>
-                  <Form.Control type = "input" id = "universidad"></Form.Control>
+                <Form.Label>Universidad</Form.Label>
+                <Form.Control
+                  type="input"
+                  id="universidad"
+                  value={formData.universidad}
+                  onChange={handleInputChange}
+                />
               </Form.Group>
-              </div>
-              
+            </div>
           </div>
-          <div class="row">
-              <div class="col">
-                  <Form.Group>
-                      <Form.Label>Carrera</Form.Label>
-                      <Form.Control type = "input" id = "carrera"></Form.Control>
-                  </Form.Group>
-              </div>
+          <div className="row">
+            <div className="col">
+              <Form.Group>
+                <Form.Label>Carrera</Form.Label>
+                <Form.Control
+                  type="input"
+                  id="carrera"
+                  value={formData.carrera}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+            </div>
           </div>
         </div>
-        <div class="linea"></div>
-        <div class="derecha_uni">
-          <div class="row">
-              <div class="col">
-                  <Form.Group>
-                      <Form.Label>Agregar Cursos</Form.Label>
-                      <Form.Control type = "input" id = "cursos"></Form.Control>
-                  </Form.Group>
-              </div>
+        <div className="linea"></div>
+        <div className="derecha_uni">
+          <div className="row">
+            <div className="col">
+              <Form.Group>
+                <Form.Label>Agregar Cursos</Form.Label>
+                <Form.Control
+                  type="input"
+                  id="cursos"
+                  value={formData.cursos}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default Universidad;
