cab4b9e26e4ab5ce4c61285b8959f33a23b23acd
frankvic03 <93057660+frankvic03@users.noreply.github.com>
2023-07-18 09:03:03 -0500

    Funcionalidad Cancelar BotonCitasCard.js

--- src/pages/VisualizarCitas/commons/botones/boton_citascard/BotonCitasCard.js
+++ src/pages/VisualizarCitas/commons/botones/boton_citascard/BotonCitasCard.js
@@ -1,10 +1,10 @@
-import { render } from "@testing-library/react";
 import {useState} from "react";
 import { Calificar } from "../../../components/calificar/Calificar";
 import './BotonCitasCard.css';
 
-function BotonCitasCard({opcion, show, setShow}){
-    //const [show,setShow] = useState(false);
+function BotonCitasCard({opcion}, citaID){
+    citaID = 9; //EJEMPLO ID DE LO QUE SE DEBE ENVIAR COMO PARAMETRO PARA ELIMINAR CITA
+    const [show,setShow] = useState(false);
 
     const ShowCard=() =>{
         if(show==true){
@@ -12,14 +12,37 @@ function BotonCitasCard({opcion, show, setShow}){
         }else{
             setShow(true);
         }
-        console.log(show)
     }
-
+    
+    const handleClickBorrarCita = (citaID) => {
+        fetch('http://localhost:3001/delete-cita/' + citaID, {
+          method: 'POST'
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data); // Verificar los datos obtenidos desde el servidor
+            
+            if (data.error) {
+                // Mostrar mensaje de error si existe
+                alert(data.error);
+              } else {
+                // Mostrar mensaje de éxito si no hay error
+                alert("Cita eliminada correctamente");
+              }
+            
+          })
+          .catch(error => console.log('Ocurrió un error:', error));
+      };
+    
     let button;
         if(opcion=="cancelar"){
-            button = <button class="button" id="cancelar">Cancelar</button>
+            
+            button = <button class="button" id="cancelar" onClick={() => handleClickBorrarCita(citaID)}>Cancelar</button>
         }else{
-            button = <button  onClick={ShowCard} class="button" id="calificar" >Calificar</button>
+            button = <button onClick={ShowCard} class="button" id="calificar" >Calificar</button>
+            {
+                show && (<Calificar/>)
+            }
         }
 
     return (
