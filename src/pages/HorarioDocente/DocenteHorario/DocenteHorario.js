6ed4f7e3c27a27f9201f6aa6a030e9946dd26847
GiGi <87880738+GiGi45@users.noreply.github.com>
2023-07-19 09:58:59 -0700

    HorarioDocente

--- src/pages/HorarioDocente/DocenteHorario/DocenteHorario.js
+++ src/pages/HorarioDocente/DocenteHorario/DocenteHorario.js
@@ -1,4 +1,4 @@
-import "./DocenteHorario.css"; //Hay un bug con los css, se debe comentar actulizar y volver a agregar para que se aplique apropiadamente el estilo
+import "./DocenteHorario.css";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
@@ -8,24 +8,27 @@ import { HorarioCard } from "../components/HorarioCard";
 
 import React, { useState } from "react";
 
-export function DocenteHorario() { 
-  const [DataFormulario, setDataFormulario] = useState({ //Iniciar variable DataFormulario para los valores del formulario
+export function DocenteHorario() {
+  const [dataFormulario, setDataFormulario] = useState({
     dia: "",
     horaInicio: "",
     horaFin: "",
     enlace: "",
   });
 
-  const handleInput = (Event) => {
-    const { name, value } = Event.target;
+  const [horarios, setHorarios] = useState([]);
+
+  const handleInput = (event) => {
+    const { name, value } = event.target;
     setDataFormulario((prevDataFormulario) => ({
       ...prevDataFormulario,
       [name]: value,
     }));
   };
 
-  const handleAgregarClick = () => { //Se hace un set a los valores del formulario al hacer click en el boton
-    console.log(DataFormulario);
+  const handleAgregarClick = () => {
+    const newHorario = { ...dataFormulario };
+    setHorarios((prevHorarios) => [...prevHorarios, newHorario]);
     setDataFormulario({
       dia: "",
       horaInicio: "",
@@ -45,7 +48,7 @@ export function DocenteHorario() {
               type="text"
               placeholder="Lunes"
               name="dia"
-              value={DataFormulario.dia}
+              value={dataFormulario.dia}
               onChange={handleInput}
             />
           </Form.Group>
@@ -58,7 +61,7 @@ export function DocenteHorario() {
               type="text"
               placeholder="9:00 am"
               name="horaInicio"
-              value={DataFormulario.horaInicio}
+              value={dataFormulario.horaInicio}
               onChange={handleInput}
             />
           </Form.Group>
@@ -71,7 +74,7 @@ export function DocenteHorario() {
               type="text"
               placeholder="11:00 am"
               name="horaFin"
-              value={DataFormulario.horaFin}
+              value={dataFormulario.horaFin}
               onChange={handleInput}
             />
           </Form.Group>
@@ -84,7 +87,7 @@ export function DocenteHorario() {
               type="text"
               placeholder="URL"
               name="enlace"
-              value={DataFormulario.enlace}
+              value={dataFormulario.enlace}
               onChange={handleInput}
             />
           </Form.Group>
@@ -95,26 +98,17 @@ export function DocenteHorario() {
           </Button>
         </Col>
       </Row>
-      <br></br>
+      <br />
 
-      <HorarioCard
-        contador="1"
-        dia="Lunes"
-        horaInicio="03:00pm"
-        horaFin="05:00pm"
-      />
-      <HorarioCard
-        contador="2"
-        dia="Martes"
-        horaInicio="04:00pm"
-        horaFin="06:00pm"
-      />
-      <HorarioCard
-        contador="3"
-        dia="Miercoles"
-        horaInicio="05:00pm"
-        horaFin="08:00pm"
-      />
+      {horarios.map((horario, index) => (
+        <HorarioCard
+          key={index}
+          contador={index + 1}
+          dia={horario.dia}
+          horaInicio={horario.horaInicio}
+          horaFin={horario.horaFin}
+        />
+      ))}
     </Container>
   );
 }
--- src/pages/HorarioDocente/components/HorarioCard.js
+++ src/pages/HorarioDocente/components/HorarioCard.js
@@ -2,23 +2,37 @@ import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
-import "./HorarioCard.css"; //Hay un bug con los css, se debe comentar actulizar y volver a agregar para que se aplique apropiadamente el estilo
+import "./HorarioCard.css";
 
-//Componete Horario-Card, es la Card (Cajita), que se debe generar despues de llenar los formularios
 export function HorarioCard(props) {
+  const [isCardVisible, setIsCardVisible] = useState(true);
+
+  const handleDeleteClick = () => {
+    setIsCardVisible(false);
+  };
+
+  if (!isCardVisible) {
+    return null;
+  }
+
   return (
     <Card className="card">
       <Card.Body className="card-row">
         <Row className="align-items-center">
-          <Col className="col-card-contador" md={1} >
+          <Col className="col-card-contador" md={1}>
             {props.contador}
           </Col>
           <Col md={11}>
             {props.dia} de {props.horaInicio} a {props.horaFin}
           </Col>
           <Col>
-            <Button className="btn-exit border-0" style={{ color: "purple", backgroundColor: "#fef7ff" }}>
+            <Button
+              className="btn-exit border-0"
+              style={{ color: "purple", backgroundColor: "#fef7ff" }}
+              onClick={handleDeleteClick}
+            >
               <b>X</b>
             </Button>
           </Col>
