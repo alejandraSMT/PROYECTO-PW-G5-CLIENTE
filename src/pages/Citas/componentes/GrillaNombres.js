10953f4aecc1d265e066edc1b0dc79a727d8e4aa
AndreaLlerenaZ2003 <andrea.llerenaz2003@gmail.com>
2023-07-19 20:09:25 -0500

    cambiosFInales Andrea

--- src/pages/Citas/componentes/GrillaNombres.js
+++ src/pages/Citas/componentes/GrillaNombres.js
@@ -5,6 +5,11 @@ import PersonaCita from './PersonaCita';
 
 const GrillaNombres = ({ personaCitas }) => {
   const isSingleCard = personaCitas.length === 1;
+  // 2. Función para manejar el clic en el card y mostrar el alert
+  const handleCardClick = (usuarioId) => {
+    alert(`Usuario ID: ${usuarioId}`);
+  };
+
 
   return (
     <div className='persona-cita-container'>
@@ -17,6 +22,8 @@ const GrillaNombres = ({ personaCitas }) => {
             nombre={personaCita.nombreCompleto}
             universidad={personaCita.nombreUniversidad}
             carrera={personaCita.nombreCarrera}
+            usuarioId={personaCita.usuarioId}
+            onClick={handleCardClick} 
           />
         </div>
       ))}
--- src/pages/Citas/componentes/PersonaCita.js
+++ src/pages/Citas/componentes/PersonaCita.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BsArrowRight } from 'react-icons/bs';
-export default function PersonaCitaFuncion({ nombre, universidad, carrera }) {
+export default function PersonaCitaFuncion({ nombre, universidad, carrera ,usuarioId, onClick}) {
   const estiloPersonaCita = {
     backgroundColor: '#E8DEF8',
     color: 'black',
@@ -22,9 +22,14 @@ export default function PersonaCitaFuncion({ nombre, universidad, carrera }) {
   const estiloParrafo = {
     margin: '0',
   };
+  
+  // Llama a la función onClick cuando se hace clic en el card
+    const handleClick = () => {
+      onClick(usuarioId);
+    };
 
   return (
-    <button className="btn" style={estiloPersonaCita}>
+    <button className="btn" style={estiloPersonaCita} onClick={handleClick}>
       <div className="persona-info" style={estiloPersonaInfo}>
         <p className="nombre" style={estiloParrafo}>{nombre}</p>
         <p className="universidad" style={estiloParrafo}>{universidad} - {carrera}</p>
