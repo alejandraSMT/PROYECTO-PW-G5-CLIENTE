094e1f29e49d42e1f4c915f837d310c7fe58b312
unknown <alfredopachecolizares23@gmail.com>
2023-07-20 23:04:25 -0500

    Pequeños cambios

--- src/pages/VisualizarCitas/commons/botones/boton_citascard/BotonCitasCard.js
+++ src/pages/VisualizarCitas/commons/botones/boton_citascard/BotonCitasCard.js
@@ -14,25 +14,30 @@ function BotonCitasCard({opcion,show, setShow,citaID,handleCitaID}){
       setIsCitaIdSet(true);
       handleCitaID(citaID)
     };
-    const handleClickBorrarCita = (citaID) => {
-        fetch('https://proyecto-pw-g5-servidor-production.up.railway.app/delete-cita/' + citaID, {
+    const handleClickBorrarCita = async (citaID) => {
+      try {
+        const response = await fetch('https://proyecto-pw-g5-servidor-production.up.railway.app/delete-cita/' + citaID, {
           method: 'POST'
-        })
-          .then(response => response.json())
-          .then(data => {
-            console.log(data); // Verificar los datos obtenidos desde el servidor
-            
-            if (data.error) {
-                // Mostrar mensaje de error si existe
-                alert(data.error);
-              } else {
-                // Mostrar mensaje de éxito si no hay error
-                alert("Cita eliminada correctamente");
-              }
-            
-          })
-          .catch(error => console.log('Ocurrió un error:', error));
-      };
+        });
+        const data = await response.json();
+        console.log(data); // Verificar los datos obtenidos desde el servidor
+    
+        if (data.error) {
+          // Mostrar mensaje de error si existe
+          alert(data.error);
+        } else {
+          // Mostrar mensaje de éxito si no hay error
+          alert("Cita eliminada correctamente");
+          // Agregar una espera de 2 segundos antes de recargar la página
+          await new Promise(resolve => setTimeout(resolve, 2000));
+          window.location.reload();
+        }
+      } catch (error) {
+        console.log('Ocurrió un error:', error);
+      }
+    };
+    
+    
     
         if(opcion=="cancelar"){
             <button class="button" id="cancelar" onClick={() => handleClickBorrarCita(citaID)}>Cancelar</button>
